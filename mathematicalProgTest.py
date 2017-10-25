3c858a5c35749007153eb79cd95e34cac993c648
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-10-25 15:22:20 -0400

    Made basic quadratic program based on test file in drake

--- mathematicalProgTest.py
+++ mathematicalProgTest.py
@@ -1,1 +1,21 @@
+from __future__ import absolute_import
+
 import pydrake
+import numpy as np
+from pydrake.solvers import mathematicalprogram as mp
+import pydrake.symbolic as sym
+
+
+if __name__ == '__main__':
+	prog = mp.MathematicalProgram()
+	x = prog.NewContinuousVariables(2, "x")
+	prog.AddLinearConstraint(x[0] >= 1)
+	prog.AddLinearConstraint(x[1] >= 1)
+	prog.AddQuadraticCost(np.eye(2), np.zeros(2), x)
+	result = prog.Solve()
+
+	print(result)
+	assert result == mp.SolutionResult.kSolutionFound
+
+	print(prog.GetSolution(x))
+	assert np.allclose(prog.GetSolution(x), np.array([1, 1]))

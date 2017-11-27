a74984f769ca0bb54166ee2cbd1068f163369c70
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2017-11-27 17:55:58 -0500

    Updated planners using qhull equations and simplices

--- footstepPlannerm3.1.py
+++ footstepPlannerm3.1.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
 	
 	# ********* HARD CODE POINTS/REGIONS OF INTEREST *********
 	# Set Goal Point
-	dim = 3
-	num_regions = 2
+	dim = 2
+	num_regions = 5
 	# Create Polytope V-representations
 	# P = []
 	# v1 = np.array([(0,0,0), (0,0,1), (0,1,0), (1,0,0), (0,1,1), (1,0,1), (1,1,0), (0.5, 0.5, 0.5), (1,1,1)]) # numpy array of vertices of A1
@@ -119,26 +119,25 @@ if __name__ == '__main__':
 	# Constrain the points to the regions
 	for i in range(num_regions):
 		for j in range(A[i].shape[0]):
-			prog.AddLinearConstraint(A[i][j][0]*x[0]+A[i][j][1]*x[1]+A[i][j][2]*x[2] + M*z[i] <= b[i][j] + M)
+			prog.AddLinearConstraint(A[i][j][0]*x[0]+A[i][j][1]*x[1] + M*z[i] <= b[i][j] + M)
 
 
 	# Add objective
-	prog.AddQuadraticCost((x[0]-x_goal[0])**2 + (x[1]-x_goal[1])**2 + (x[2]-x_goal[2])**2) # distance of x to the goal point
+	prog.AddQuadraticCost((x[0]-x_goal[0])**2 + (x[1]-x_goal[1])**2) # distance of x to the goal point
 
 	# Solve problem
 	solver = GurobiSolver()
 	assert(solver.available())
 	assert(solver.solver_type()==mp.SolverType.kGurobi)
 	result = solver.Solve(prog)
-	# assert(result == mp.SolutionResult.kSolutionFound)
-	# print("Goal: " + str(x_goal))
-	# finalx = prog.GetSolution(x)
-	# print("Final Solution: " + str(finalx))
+	assert(result == mp.SolutionResult.kSolutionFound)
+	print("Goal: " + str(x_goal))
+	finalx = prog.GetSolution(x)
+	print("Final Solution: " + str(finalx))
 
 	# ********* GRAPH PROBLEM *********
 	# Create figure
 	fig = plt.figure(1, (20, 10))
-	ax = fig.add_subplot(111, projection='3d')
 	plt.title("Minimize distance of point within " + str(num_regions) + " " + str(dim) + "-D Polytopes to Goal Point")
 
 	# Plot regions
@@ -147,10 +146,10 @@ if __name__ == '__main__':
 		for simplex in chulls[j].simplices:
 			print(simplex)
 			print(chulls[j].points[simplex])
-			plt.plot(chulls[j].points[simplex, 0], chulls[j].points[simplex, 1], chulls[j].points[simplex, 2], 'b')
+			plt.plot(chulls[j].points[simplex, 0], chulls[j].points[simplex, 1], 'b')
 
-	# plt.plot([finalx[0]], [finalx[1]], 'g*', markersize=15, markerfacecolor='g') # solution
-	# plt.annotate("SOL: (" + str(round(finalx[0], 3)) + ", " + str(round(finalx[1], 3)) + ")", xy=(finalx[0], finalx[1]))
-	plt.plot([x_goal[0]], [x_goal[1]], [x_goal[2]], 'r*', markersize=15, markerfacecolor='r') # goal
+	plt.plot([finalx[0]], [finalx[1]], 'g*', markersize=15, markerfacecolor='g') # solution
+	plt.annotate("SOL: (" + str(round(finalx[0], 3)) + ", " + str(round(finalx[1], 3)) + ")", xy=(finalx[0], finalx[1]))
+	plt.plot([x_goal[0]], [x_goal[1]], 'r*', markersize=15, markerfacecolor='r') # goal
 	plt.annotate("GOAL: (" + str(round(x_goal[0], 3)) + ", " + str(round(x_goal[1], 3)) + ")", xy=(x_goal[0], x_goal[1]))
 	plt.show() # Show plot

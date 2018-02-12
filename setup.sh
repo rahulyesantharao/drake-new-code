34925fef8db3184be4b3c5f63da16cb9ddfac748
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-02-12 16:14:04 -0500

    Created setup files

---
+++ setup.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/home/rahuly/Data/drake-new/drake-distro/install/lib/python2.7/site-packages
+export LD_LIBRARY_PATH=/home/rahuly/Data/drake-new/drake-distro/install/lib:/home/rahuly/Data/gurobi702/linux64/lib

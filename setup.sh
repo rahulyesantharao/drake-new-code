dafb894b4ef6d6e9b61630b5a02c7c3bb709aa22
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-04-09 14:44:58 -0400

    Added pythonpath in setup to the new build pyhton path

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=/home/rahuly/Data/drake-new/drake-distro/install/lib/python2.7/site-packages
+#export PYTHONPATH=/home/rahuly/Data/drake-distro/install/lib/python2.7/site-packages
 export LD_LIBRARY_PATH=/home/rahuly/Data/drake-new/drake-distro/install/lib:/home/rahuly/Data/gurobi702/linux64/lib
 export GUROBI_PATH=/home/rahuly/Data/gurobi702/linux64

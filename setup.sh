772f461c949bf923713b99c234f26e3db5f6c3e2
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-05-04 16:25:20 -0400

    Added some variables for new Drake

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-export PYTHONPATH=/home/rahuly/Data/drake-new/drake-distro/install/lib/python2.7/site-packages
-#export PYTHONPATH=/home/rahuly/Data/drake-distro/install/lib/python2.7/site-packages
-export LD_LIBRARY_PATH=/home/rahuly/Data/drake-new/drake-distro/install/lib:/home/rahuly/Data/gurobi702/linux64/lib
+#export PYTHONPATH=/home/rahuly/Data/drake-new/drake-distro/install/lib/python2.7/site-packages
+export PYTHONPATH=/home/rahuly/Data/drake-distro/install/lib/python2.7/site-packages
+#export LD_LIBRARY_PATH=/home/rahuly/Data/drake-new/drake-distro/install/lib:/home/rahuly/Data/gurobi702/linux64/lib
 export GUROBI_PATH=/home/rahuly/Data/gurobi702/linux64
+export GUROBI_PATH=/home/rahuly/Data/gurobi752/linux64
+export GRB_LICENSE_FILE=/home/rahuly/gurobi.lic
